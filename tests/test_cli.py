e52175690725b007d92f1004e30a62a34ce2c0c9
Stefan Hagen <stefan@hagen.link>
2020-09-06 21:00:03 +0200

    Fixed another test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -44,7 +44,24 @@ def test_main_ok_gcc_data(capsys):
 def test_main_nok_direct_non_gcc_text_gcc_code(capsys):
     job = ['<style> (CWE-0) <<<The column 13 causes always trouble in line 42.>>> [/a/path/file.ext:42] -> [/a/path/file.ext:222]']
     report_expected = (
-        ''
+        '{"version": "2.1.0", "runs": [{"tool": {"driver": {"name": "RTSL!", '
+        '"fullName": "Read the Source, Luke!", "version": "2020.09", "rules": [{"id": '
+        '"CWE1350", "name": "CWE VIEW: Weaknesses in the 2020 CWE Top 25 Most '
+        'Dangerous Software Weaknesses", "helpUri": '
+        '"https://cwe.mitre.org/data/definitions/1350.html"}]}}, "conversion": '
+        '{"tool": {"driver": {"name": "scars_to_sarif"}}, "invocation": {"arguments": '
+        '["--"], "executionSuccessful": true, "commandLine": "--", "endTimeUtc": '
+        '"2020-09-08T12:34:56Z", "workingDirectory": {"uri": "/home/ci/transform"}}}, '
+        '"invocations": [{"executionSuccessful": true, "endTimeUtc": '
+        '"2020-09-08T12:34:57Z", "workingDirectory": {"uri": "/home/ci/transform"}}], '
+        '"versionControlProvenance": [{"repositoryUri": '
+        '"https://ci.example.com/project/repo/", "revisionId": "cafefade", "branch": '
+        '"default"}], "properties": {"metrics": {"total": 1, "error": 1, "warning": '
+        '0}}, "results": []}], "$schema": '
+        '"https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json", '
+        '"inlineExternalProperties": [{"guid": '
+        '"0c9fe04f-9b74-4972-a82e-2099710a0ba1", "runGuid": '
+        '"dce1bdf0-358b-4898-bedf-f297160f3b37"}]}'
     )
     assert cli.main(job, True) == 0
     out, err = capsys.readouterr()
