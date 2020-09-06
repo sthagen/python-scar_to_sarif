07e525ba299c37bf5e2ab44a2d3add735e38d11b
Stefan Hagen <stefan@hagen.link>
2020-09-06 21:20:26 +0200

    Added SARIF v2.1.0 schema to test fixtures and a first fixture test to validate the sample report

---
+++ tests/test_fixtures.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import json
+import sys
+
+import pytest  # type: ignore
+
+import jsonschema  # type: ignore
+
+ENCODING = "utf-8"
+SARIF_JSON_SCHEMA = 'tests/fixtures/sarif-schema-2.1.0.json'
+SARIF_REPORT_SAMPLE = 'tests/fixtures/some-report.sarif.json'
+
+
+def _load(file_path):
+    """Create JSON object from file."""
+    with open(file_path, "rt", encoding=ENCODING) as handle:
+        return json.load(handle)
+
+
+def _validate(document, schema, conformance=None):
+    """Validate the document against the schema."""
+    conformance = conformance if conformance else jsonschema.draft7_format_checker
+    return jsonschema.validate(document, schema, format_checker=conformance)
+
+
+def test_sarif_fixture(capsys):
+
+    schema = _load(SARIF_JSON_SCHEMA)
+    document = _load(SARIF_REPORT_SAMPLE)
+
+    assert _validate(document, schema) is None
+    out, err = capsys.readouterr()
+    assert out.strip() == ''
